df5371bac54d87b2d780fe5c0b1738cd48858017
Jurij Ambros <jurij.ambros@gmail.com>
2021-03-20 08:07:19 +0200

    užduotys

---
+++ Db_uzduotys.sql
@@ -0,0 +1,22 @@
+#1. Atvaizduoti visus atlikėjus, kurie turi slapyvardžius
+
+
+#2. Atvaizduoti albumus, kurie yra 'Rock' žanro
+
+
+#3. Atvaizduoti albumus pagal leidimo datą nuo seniausio
+
+
+#4. Atvaizduoti Dainą ir albumo pavadinimas, kurių dainos pavadinime yra raidės 'ki'
+
+
+#5. Atvaizduoti atlikėjo vardą, pavardę, albumų skaičių
+
+
+#6. Atvaizduoti kiek vidutiniškai trunka kiekvieno albumo dainos
+
+
+#7. Atvaizduoti vidutinį kiekvieno albumo vertinimą
+
+
+#8. Atvaizduoti kiek atlikėjas sukūręs dainų
